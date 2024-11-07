e370a762a6555d41c2a49a877411ffa6f2772936
fabianazh <fabianazhar216@gmail.com>
2024-11-08 06:01:15 +0700

    Updating HMSI logo

--- src/components/Section/Memories.tsx
+++ src/components/Section/Memories.tsx
@@ -20,7 +20,7 @@ export default function Memories() {
             </span>
             {/* End Title */}
             {/* Content */}
-            <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6 px-10 lg:px-0">
+            <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6 px-5 lg:px-0">
                 {memories.map((memory: Memory, index: number) => (
                     <div
                         key={index}
